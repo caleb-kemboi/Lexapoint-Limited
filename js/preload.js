ea21ddf6543db6d092d942d845df5b3cc0272c01
ck <ck@gmail.com>
2021-06-07 15:07:37 +0300

    adding files

---
+++ js/preload.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+     $("#loading-center").click(function() {
+     $("#loading").fadeOut(500);
+     })    
+     $(window).load(function() {
+   $("#loading").fadeOut(500);
+})    
+});
